f123a975a2ad3844018fa5866ef842dfca00d01b
pqdong <dong.phung@tiki.vn>
2019-05-06 15:10:03 +0700

    build

---
+++ letter-matching-game/precache-manifest.a8f43535800e44f291718acd9a1ab811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4a3ee5e608b667167b5552696c1a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab41c1a72726ae2b7cca",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "5ac82480ae3f5f4b94f4",
+    "url": "/static/css/main.9aa5548d.chunk.css"
+  },
+  {
+    "revision": "ab41c1a72726ae2b7cca",
+    "url": "/static/js/2.e733294a.chunk.js"
+  },
+  {
+    "revision": "5ac82480ae3f5f4b94f4",
+    "url": "/static/js/main.fbf51aaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
